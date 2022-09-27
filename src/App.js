cf66ac61f2d070ce01ccdf47dc4600acd1e22e01
Lawrence <lhollis@youniqueproducts.com>
2022-09-27 17:24:45 -0600

    fixing file names, npm audit fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React        from 'react';
 import { Title }    from "./components/Title";
-import { Info }     from "./components/info";
+import { Info }     from "./components/Info";
 import { Demo }     from "./components/Demo";
 
 function App() {
